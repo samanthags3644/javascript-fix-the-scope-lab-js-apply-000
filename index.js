a45c37b2c9a771f8a9b51b82372ee309d33cbdfd
Samantha GarciaSilva <samanthags3644@gmail.com>
2018-07-18 03:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  return (functionName(cat))
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
